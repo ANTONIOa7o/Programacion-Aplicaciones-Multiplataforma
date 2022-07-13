e7193f92c7a8ddf896bd45c9f16deaf36f127658
Antonio <78384046+ANTONIOa7o@users.noreply.github.com>
2022-07-13 16:12:19 +0200

    Delete Angula_I/ejemplo1-presentacion/ejemplo2-AngularWorld directory

--- Angula_I/ejemplo1-presentacion/ejemplo2-AngularWorld/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true
-};
-/*
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-Será aqui,donde conectaremos con  el servicio que nos haga el hueco al servicio de 
-el servidor
-*/
