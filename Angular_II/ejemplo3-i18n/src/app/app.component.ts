971a7ba0439ecbdbaac97bc30840807a15f11dea
Antonio <78384046+ANTONIOa7o@users.noreply.github.com>
2022-07-21 09:26:48 +0200

    Add files via upload

---
+++ Angular_II/ejemplo3-i18n/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  title = 'ejemplo3-i18n';
+  idioma: string = 'es'
+
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.use(this.idioma);
+
+  }
+  cambiarIdioma(nuevoIdioma: string) {
+    /* this.idioma = nuevoIdioma; */
+    this.translateService.use(nuevoIdioma);
+  }
+}
